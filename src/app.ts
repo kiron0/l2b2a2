3cff1ce6ddfbd356da29a3d1cf678ee1cb01a5bc
Toufiq Hasan Kiron <toufiqhasankiron2@gmail.com>
2023-11-24 23:14:43 +0600

    updated readme

--- src/app.ts
+++ src/app.ts
@@ -16,7 +16,7 @@ app.set('view engine', 'ejs')
 // Set the path to the views directory
 app.set('views', path.join(__dirname, '../views'))
 
-//parser
+// parser
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
